cbc37b5795d405cbea01bcdf0e507a865c98dea8
hongyt <hongyt@uw.edu>
2021-11-02 09:33:07 -0700

    first gen navbar and page routers

---
+++ infodawgs/src/components/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css";
+
+function home() {
+  return (
+    <div>
+      <h1>Home page code goes here</h1>
+    </div>
+  );
+}
+
+export default home;
---
+++ infodawgs/src/components/pages/resources.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./resources.css";
+
+function resources() {
+  return (
+    <div>
+      <h1>Resources page code goes here</h1>
+    </div>
+  );
+}
+
+export default resources;
