62c25784d3d989b2f4d46daf19e6ff82f8bb1679
Wendell Li <wendellli1999@gmail.com>
2021-11-02 21:33:34 -0700

    Nothing important

--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./calculator.css";
+import FoodPicker from "../components/FoodPicker";
 
 function calculator() {
   return (
     <div>
       <h1>Here you can calculate the emissions of your foods and drinks!</h1>
+      <div>
+        <div className="flex-container">
+          <div className="row">
+            <FoodPicker />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
