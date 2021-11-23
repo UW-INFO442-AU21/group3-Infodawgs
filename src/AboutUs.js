677d82f193d427cf62e2da7b45b594124956caa2
Wendell Li <wendellli1999@gmail.com>
2021-11-23 08:48:05 -0800

    Add more sections

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -60,6 +60,218 @@ class AboutUs extends Component {
                   <li>Class of 2022</li>
                 </ul>
               </section>
+              <section className="flex-item-4">
+                <img
+                  className="rounded-circle profile"
+                  src={`${process.env.PUBLIC_URL}/wendell.jpg`}
+                  alt="people"
+                />
+                <h2 className="name">Wendell Li</h2>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/wendellli99/"
+                  role="button"
+                  aria-label="LinkedIn"
+                >
+                  <img
+                    alt="LinkedIn"
+                    src={`${process.env.PUBLIC_URL}/icons/linkedin.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.facebook.com/profile.php?id=100005237762480/"
+                  role="button"
+                  aria-label="Facebook"
+                >
+                  <img
+                    alt="Facebook"
+                    src={`${process.env.PUBLIC_URL}/icons/facebook.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="mailto:wendelli@uw.edu"
+                  role="button"
+                  aria-label="E-mail"
+                >
+                  <img
+                    alt="E-mail"
+                    src={`${process.env.PUBLIC_URL}/icons/email.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <ul className="list">
+                  <li>UI/UX Designer</li>
+                  <li>iOS App Game Developer</li>
+                  <li>Student at the University of Washington</li>
+                  <li>Class of 2022</li>
+                </ul>
+              </section>
+              <section className="flex-item-4">
+                <img
+                  className="rounded-circle profile"
+                  src={`${process.env.PUBLIC_URL}/wendell.jpg`}
+                  alt="people"
+                />
+                <h2 className="name">Wendell Li</h2>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/wendellli99/"
+                  role="button"
+                  aria-label="LinkedIn"
+                >
+                  <img
+                    alt="LinkedIn"
+                    src={`${process.env.PUBLIC_URL}/icons/linkedin.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.facebook.com/profile.php?id=100005237762480/"
+                  role="button"
+                  aria-label="Facebook"
+                >
+                  <img
+                    alt="Facebook"
+                    src={`${process.env.PUBLIC_URL}/icons/facebook.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="mailto:wendelli@uw.edu"
+                  role="button"
+                  aria-label="E-mail"
+                >
+                  <img
+                    alt="E-mail"
+                    src={`${process.env.PUBLIC_URL}/icons/email.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <ul className="list">
+                  <li>UI/UX Designer</li>
+                  <li>iOS App Game Developer</li>
+                  <li>Student at the University of Washington</li>
+                  <li>Class of 2022</li>
+                </ul>
+              </section>
+              <section className="flex-item-4">
+                <img
+                  className="rounded-circle profile"
+                  src={`${process.env.PUBLIC_URL}/wendell.jpg`}
+                  alt="people"
+                />
+                <h2 className="name">Wendell Li</h2>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/wendellli99/"
+                  role="button"
+                  aria-label="LinkedIn"
+                >
+                  <img
+                    alt="LinkedIn"
+                    src={`${process.env.PUBLIC_URL}/icons/linkedin.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.facebook.com/profile.php?id=100005237762480/"
+                  role="button"
+                  aria-label="Facebook"
+                >
+                  <img
+                    alt="Facebook"
+                    src={`${process.env.PUBLIC_URL}/icons/facebook.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="mailto:wendelli@uw.edu"
+                  role="button"
+                  aria-label="E-mail"
+                >
+                  <img
+                    alt="E-mail"
+                    src={`${process.env.PUBLIC_URL}/icons/email.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <ul className="list">
+                  <li>UI/UX Designer</li>
+                  <li>iOS App Game Developer</li>
+                  <li>Student at the University of Washington</li>
+                  <li>Class of 2022</li>
+                </ul>
+              </section>
+              <section className="flex-item-4">
+                <img
+                  className="rounded-circle profile"
+                  src={`${process.env.PUBLIC_URL}/wendell.jpg`}
+                  alt="people"
+                />
+                <h2 className="name">Wendell Li</h2>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/wendellli99/"
+                  role="button"
+                  aria-label="LinkedIn"
+                >
+                  <img
+                    alt="LinkedIn"
+                    src={`${process.env.PUBLIC_URL}/icons/linkedin.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.facebook.com/profile.php?id=100005237762480/"
+                  role="button"
+                  aria-label="Facebook"
+                >
+                  <img
+                    alt="Facebook"
+                    src={`${process.env.PUBLIC_URL}/icons/facebook.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="mailto:wendelli@uw.edu"
+                  role="button"
+                  aria-label="E-mail"
+                >
+                  <img
+                    alt="E-mail"
+                    src={`${process.env.PUBLIC_URL}/icons/email.jpg`}
+                    className="contact"
+                  />
+                </a>
+                <ul className="list">
+                  <li>UI/UX Designer</li>
+                  <li>iOS App Game Developer</li>
+                  <li>Student at the University of Washington</li>
+                  <li>Class of 2022</li>
+                </ul>
+              </section>
             </div>
           </div>
         </div>
