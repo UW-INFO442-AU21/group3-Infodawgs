a22095402c91f84f62b29e2398a992db5de2b7f5
Wendell Li <wendellli1999@gmail.com>
2021-11-23 11:01:15 -0800

    Add resources

--- src/ResourcePage.js
+++ src/ResourcePage.js
@@ -1,37 +1,94 @@
 import React from "react";
 
-
 const Resources = () => {
-    return (
-        <div>
-            <div>
-                <h2>
-                    Other Resources on Sustainable Eating
-                </h2>
-                <p>
-                    Here are some extra resources that you could use to understand more about the implications our diet has on the
-                    environment. Feel free to explore and learn more about what you can do for the environment!
-                </p>
+  return (
+    <div>
+      <div>
+        {/* <div class="main-resource">
+          <h2>Other Resources on Sustainable Eating</h2>
+          <p>
+            Here are some extra resources that you could use to understand more
+            about the implications our diet has on the environment. Feel free to
+            explore and learn more about what you can do for the environment!
+          </p>
+        </div> */}
+        <ul class="cards-resource ">
+          <li class="cards_item-resource ">
+            <div class="card-resource ">
+              <div class="card_image-resource ">
+                <img src="https://picsum.photos/500/300/?image=10" />
+              </div>
+              <div class="card_content-resource ">
+                <h2 class="card_title-resource ">Card Grid Layout</h2>
+                <p class="card_text-resource ">TBD</p>
+                <button class="btn card_btn-resource ">Read More</button>
+              </div>
             </div>
-
-            <div className="flex-container">
-                <div className="flex-item">
-                    
-                </div>
-                <div className="flex-item">
-                    
-                </div>
-                <div className="flex-item">
-                    
-                </div>
-
-            <p>
-                Placeholder for now. WIP. 
-            </p>
+          </li>
+          <li class="cards_item-resource ">
+            <div class="card-resource ">
+              <div class="card_image-resource ">
+                <img src="https://picsum.photos/500/300/?image=5" />
+              </div>
+              <div class="card_content-resource ">
+                <h2 class="card_title-resource ">Card Grid Layout</h2>
+                <p class="card_text-resource ">TBD</p>
+                <button class="btn card_btn-resource ">Read More</button>
+              </div>
             </div>
-        </div>
-        
-    );
+          </li>
+          <li class="cards_item-resource ">
+            <div class="card-resource ">
+              <div class="card_image-resource ">
+                <img src="https://picsum.photos/500/300/?image=11" />
+              </div>
+              <div class="card_content-resource ">
+                <h2 class="card_title-resource ">Card Grid Layout</h2>
+                <p class="card_text-resource ">TBD</p>
+                <button class="btn card_btn-resource ">Read More</button>
+              </div>
+            </div>
+          </li>
+          <li class="cards_item-resource ">
+            <div class="card-resource ">
+              <div class="card_image-resource ">
+                <img src="https://picsum.photos/500/300/?image=14" />
+              </div>
+              <div class="card_content-resource ">
+                <h2 class="card_title-resource ">Card Grid Layout</h2>
+                <p class="card_text-resource ">TBD</p>
+                <button class="btn card_btn-resource ">Read More</button>
+              </div>
+            </div>
+          </li>
+          <li class="cards_item-resource ">
+            <div class="card-resource ">
+              <div class="card_image-resource ">
+                <img src="https://picsum.photos/500/300/?image=17" />
+              </div>
+              <div class="card_content-resource ">
+                <h2 class="card_title-resource ">Card Grid Layout</h2>
+                <p class="card_text-resource ">TBD</p>
+                <button class="btn card_btn-resource ">Read More</button>
+              </div>
+            </div>
+          </li>
+          <li class="cards_item-resource ">
+            <div class="card-resource ">
+              <div class="card_image-resource ">
+                <img src="https://picsum.photos/500/300/?image=2" />
+              </div>
+              <div class="card_content-resource ">
+                <h2 class="card_title-resource ">Card Grid Layout</h2>
+                <p class="card_text-resource ">TBD</p>
+                <button class="btn card_btn-resource ">Read More</button>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Resources;
