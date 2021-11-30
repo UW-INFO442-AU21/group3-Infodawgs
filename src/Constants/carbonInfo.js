fb9e21b80fcbc51aceef1ac6d46e5c36e4dc8a7f
benchanhw <benchanhw@gmail.com>
2021-11-30 09:58:41 -0800

    updated the calculator messages with comparison data

--- src/Constants/carbonInfo.js
+++ src/Constants/carbonInfo.js
@@ -2,31 +2,31 @@ export const carbonInfo = [
   {
     level: "Low",
     levelColor: "carbon-level-low",
-    description: "Your carbon emission level is 'Low'.",
+    description: "Your carbon emission level is well below the US average, congrats! For context, your emissions are equivalent to about 8kWh of natural gas, or enough electrcity to run your oven for 2.5 hours.",
     arrow: "lowcarbonArrow",
   },
   {
     level: "Low Moderate",
     levelColor: "carbon-level-low-moderate",
-    description: "Your carbon emission is 'Low Moderate'.",
+    description: "Your carbon emission is below the US average! For context, your emissions would be taken out of the air by a tree in 6 months.",
     arrow: "lowModeratecarbonArrow",
   },
   {
     level: "Moderate",
     levelColor: "carbon-level-moderate",
-    description: "Your carbon emission is 'Moderate'.",
+    description: "Your carbon emission is around the US average. For context, your emissions are equivalent to driving a car for 30 miles.",
     arrow: "moderatecarbonArrow",
   },
   {
     level: "Moderate High",
     levelColor: "carbon-level-moderate-high",
-    description: "Your carbon level is 'Moderate High'.",
+    description: "Your carbon level above the US average. Just for context, your emissions are equivalent to the energy use of your phone for an entire year.",
     arrow: "moderateHighcarbonArrow",
   },
   {
     level: "High",
     levelColor: "carbon-level-high",
-    description: "Your carbon level is 'High'.",
+    description: "Your carbon level is well above the US average, oh dear. For context, your emissions are similar to taking a 10 lb lump of coal and burning it all.",
     arrow: "highcarbonArrow",
   },
 ];
